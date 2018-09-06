5e813bb763ee4c0b0fc31f6f52b52a9b87496fed
AndrewMallach <andrew.mallach1@marist.edu>
2018-09-06 14:56:31 -0400

    Solution to Curiosity rover excersise.

---
+++ rover.py
@@ -0,0 +1,11 @@
+# CMPT 120L 113
+# Mars Rover Program
+# Author: Andrew Mallach
+# Created: 9/6/18
+
+def mars_to_earth():
+        time = (34000000/186000)
+        print ("The time it takes to send a photo from the Mars Curiosity Rover to NASA back on Earth is about" + " " + str(time) + " " + "seconds.")
+mars_to_earth()
+
+
