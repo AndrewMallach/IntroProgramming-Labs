88f743435bb6474a29b309571353266b47267328
AndrewMallach <andrew.mallach1@marist.edu>
2018-09-06 15:16:44 -0400

    Solution to Mad Libs excersise.

---
+++ madlib.py
@@ -0,0 +1,9 @@
+def madlib():
+    noun = input ("Enter a noun ")
+    verb = input ("Enter a verb ")
+    adj = input ("Enter an adjective ")
+    place = input ("Enter a place ")
+
+    print("Take your " + adj + " " + noun + " and " + verb + " it at the " + place + "!")
+
+madlib()    
