f882dc944e18e6dbb9ae976ea05112b99aabaadf
AndrewMallach <andrew.mallach1@marist.edu>
2018-09-17 13:17:43 -0400

    First commit for Game version 0.1. Replaced original text with my own story

---
+++ CMPT120-Project-1-master/Game.py
@@ -0,0 +1,100 @@
+# CMPT 120L 113
+# Andrew Mallach
+# 10 Sep 2018
+###
+
+# show game title
+print("\nMarist Mayhem\n"
+      "\n========\n")
+
+# show game introduction
+print("The day has finally arrived. August 27th, 2018. After one last"
+      " grueling year at high school, AP exams, and standardized tests,"
+      " You have finally made it to college. It only gets better from here."
+      " You finally feel like your life is in your own hands. You feel"
+      " like you are finally an adult. As the sun rises, you start to"
+      " frantically look over your schedule, anxious about the fact that"
+      " your new chapter in your academic career is about to begin. In"
+      " your nervousness however, you fail to realize that your only class"
+      " today starts at 2 PM. You decide to go back to bed, browsing social"
+      " media and still worrying about the coming day. You decide to try"
+      " and take it easy for a bit and take a nap.")
+
+# prompt the user
+input("\n<Press Enter to continue...>\n")
+
+# show current situation
+print("Oh no! You've really messed up now! "
+          "It's only the first day of school and"
+          "you overslept for your first class,"
+      "and it's an afternoon class! Not many"
+      "excuses you can come up with to get out"
+      "of that. Regardless, you think that"
+      "you might get some slack because it's"
+      "only the first day, and you head off to class.")
+
+# show the current score
+print("Your score is 0.")
+
+# prompt the user
+input("\n<Press Enter to continue...>\n")
+
+# show the current location
+print("You show up about 10 minutes late. The teacher doesn't seem "
+      "too pleased. You tell her that you were trying to find the right"
+      "room for a while, and the teacher buys it. You spend the rest"
+      "of the class time as normal.")
+
+# show the current score
+print("Your score is 5.")
+
+# same as above
+input("\n<Press Enter to continue...>\n")
+
+# same as above
+print("You go back to your dorm feeling guilty about what"
+      "happened. You were told that you have a quiz in the class"
+      "next week. You told yourself that you can't mess around"
+      "with your sleep schedule anymore.")
+
+# same as above
+print("Your score is 10.")
+
+# again...
+input("\n<Press Enter to continue...>\n")
+
+# again...
+print("Thursday arrives. You have been diligently keeping up"
+      "with all of your other classes. You start to learn more"
+      "material for this upcoming quiz for Monday, and you feel"
+      "like you can ace it given time.")
+
+# again...
+print("Your score is 15.")
+
+# yet again...
+input("\n<Press Enter to continue...>\n")
+
+# yet again...
+print("Monday arrives. You didn't study for a single minute. You spent"
+      "all of your time playing video games and going on your phone"
+      "You try to get your roommate to help you before the quiz, but"
+      "he doesn't even have the same class. You decide the only possible"
+      "way to do well is to take pictures of the textbook with your phone"
+      "and use it during the exam. That way, the material is readily"
+      "available to use. You go to class, ready to cheat.")
+
+# yet again...
+print("Your score is 20.")
+
+# one last time...
+input("\n<Press Enter to continue...>\n")
+
+# show game ending
+print("You were caught cheating by your teacher. You didn't even try to hide"
+      "the fact that you were using your phone under the table. You meet with"
+      "the college President and are promptly kicked out. Game Over!")
+
+# show credits
+print("\nCopyright (c) 2016-2018 Andrew Mallach, andrew.mallach1@marist")
+
